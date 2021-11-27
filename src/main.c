bdc672043189d3af94f1278444d55347202598b1
mmanigrasso <m.manigrasso@outlook.com>
2021-11-27 15:32:26 +0100

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,22 @@
+/**
+ * @file Excercise16_bipartiteness
+ * @author Marco Manigrasso (mamanigrasso@edu.aau.at)
+ * @brief This programm will check the bipartiteness of a given graph by coloring the vertices in two different colors.
+ * @version 1.0
+ * @date 2021-11-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+ #include <iostream>
+ 
+ #define BLUE 0
+ #define RED 1
+
+ using namespace std;
+
+ int main()
+ {
+     
+ }
